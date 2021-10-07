e7bb42073542d5e650c181d230f0226c0180b4fb
leeyunjai82 <leeyunjai@circul.us>
2021-10-07 14:34:51 +0900

    update

--- edu/device_thread.py
+++ edu/device_thread.py
@@ -1,16 +1,20 @@
 import time
-
 from openpibo.edu_v1 import Pibo
 
 def msg_device(msg):
     print(msg)
+    if "touch" in msg:
+      pibo.eye_on("red")
+    else:
+      pibo.eye_off()
+
 
 def device_thread_test():
-    pibo = Pibo()
     ret=pibo.start_devices(msg_device)
     print(ret)
     time.sleep(12)
     pibo.stop_devices()
 
 if __name__ == "__main__":
+    pibo = Pibo()
     device_thread_test()
